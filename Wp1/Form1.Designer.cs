0e7dd9b5b14830d845b08c6b6c1be2da2c52a3fc
Admin <Admin@PC38>
2020-11-18 19:39:09 +0800

    加入專案檔案。

---
+++ Wp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Wp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.go = new System.Windows.Forms.Button();
+            this.lb1 = new System.Windows.Forms.Label();
+            this.tB1 = new System.Windows.Forms.TextBox();
+            this.pB1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // go
+            // 
+            this.go.Location = new System.Drawing.Point(200, 50);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(100, 30);
+            this.go.TabIndex = 0;
+            this.go.Text = "查詢";
+            this.go.UseVisualStyleBackColor = true;
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("新細明體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lb1.Location = new System.Drawing.Point(0, 0);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(267, 32);
+            this.lb1.TabIndex = 1;
+            this.lb1.Text = "2021連價查詢系統";
+            // 
+            // tB1
+            // 
+            this.tB1.Font = new System.Drawing.Font("新細明體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tB1.Location = new System.Drawing.Point(20, 50);
+            this.tB1.Name = "tB1";
+            this.tB1.Size = new System.Drawing.Size(100, 30);
+            this.tB1.TabIndex = 2;
+            // 
+            // pB1
+            // 
+            this.pB1.Location = new System.Drawing.Point(50, 100);
+            this.pB1.Name = "pB1";
+            this.pB1.Size = new System.Drawing.Size(250, 250);
+            this.pB1.TabIndex = 3;
+            this.pB1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.pB1);
+            this.Controls.Add(this.tB1);
+            this.Controls.Add(this.lb1);
+            this.Controls.Add(this.go);
+            this.Name = "Form1";
+            this.Text = "2021連價查詢系統";
+            ((System.ComponentModel.ISupportInitialize)(this.pB1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button go;
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.TextBox tB1;
+        private System.Windows.Forms.PictureBox pB1;
+    }
+}
+
